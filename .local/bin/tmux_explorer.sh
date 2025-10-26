cca8c90349a2448ab78d148976c75a7c95f34b74
AlexTrebs <snalex2539@gmail.com>
2025-10-26 19:09:11 +0000

    Reduced file explorer to one script. Added check for file on right arrow

--- .local/bin/tmux_explorer.sh
+++ .local/bin/tmux_explorer.sh
@@ -116,10 +116,13 @@ LEFT() {
 }
 
 RIGHT() {
-  [[ -z "$selection" ]] && return
-  local path="$selection"
-  [[ "$path" != /* ]] && path="$BASE_DIR/$path"
-  [[ -d "$path" ]] && current_dir="$path"
+  if [[ -f "$selection" ]]; then
+    current_dir="$current_dir"
+  else
+    local path="$selection"
+    [[ "$path" != /* ]] && path="$BASE_DIR/$path"
+    [[ -d "$path" ]] && current_dir="$path"
+  fi
 }
 
 ENTER() {
@@ -185,4 +188,21 @@ explore() {
   done
 }
 
-explore
+# if called as: tmux-folder-explorer --explore
+if [[ "${1:-}" == "--explore" ]]; then
+  explore
+  exit 0
+fi
+
+current_dir="$(tmux display-message -p -F "#{pane_current_path}")"
+if tmux list-windows -a | grep -E -q '^[^:]+:[0-9]+:[[:space:]]+explorer[*-]?'; then
+  # Extract the session name that has it
+  session_name="$(tmux list-windows -a | grep -E '^[^:]+:[0-9]+:[[:space:]]+explorer[*-]?' | head -n1 | cut -d: -f1)"
+
+  # Change its directory and switch to it
+  tmux switch-client -t "$session_name"
+  tmux select-window -t "${session_name}:explorer"
+else
+  # Create new explorer window in current session, current dir
+  tmux new-window -n explorer -c "$current_dir" "$0 --explore"
+fi
